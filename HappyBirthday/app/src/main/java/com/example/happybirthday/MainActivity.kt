6f17112e1b2daa8cb14611830a5ad3c4c348f5aa
pcodesdev <njugunapm23@gmail.com>
2023-08-18 14:34:59 +0300

    have created two text field and put them in  a column

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.magnifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingText(message = "Happy Birthday Ann!", from = "from Peter")
                 }
             }
         }
@@ -31,10 +38,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier){
-Text(
-    text = message
-)
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(28.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 90.sp,
+            lineHeight = 106.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 
@@ -42,6 +64,6 @@ Text(
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-    GreetingText(message = "Happy Birthday Ann!")
+    GreetingText(message = "Happy Birthday Ann!", from = "from Peter")
     }
 }
