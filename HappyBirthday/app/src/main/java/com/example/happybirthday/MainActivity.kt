fb9dbaf1d790143879c993a5207be4f679741c07
pcodesdev <njugunapm23@gmail.com>
2023-08-19 12:31:08 +0300

    have finished adding bg image

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,7 +1,8 @@
 
 package com.example.happybirthday
-import androidx.compose.foundation.Image
 
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +37,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    GreetingText(message = "Happy Birthday Ann!", from = "from Peter")
-                    GreetingImage(message = "Happy Birthday Ann!", from = "from Peter")
+                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
+                        R.string.source_text
+                    ))
                 }
             }
         }
@@ -59,7 +63,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -73,9 +77,11 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     //Step 3 create a box to overlap image and texts
     Box {
         Image(
-            painter = image,
-            contentDescription = null
-        )
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+            alpha = 0.5F
+    )
         GreetingText(
             message = message,
             from = from,
@@ -93,6 +99,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
 //    GreetingText(message = "Happy Birthday Ann!", from = "from Peter")
-        GreetingImage(message = "Happy Birthday Ann!", from = "from Peter")
+        GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
+            R.string.source_text
+        ))
     }
 }
